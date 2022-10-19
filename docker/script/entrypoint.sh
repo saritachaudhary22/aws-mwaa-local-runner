280c98a88ccd0ab0a0fca241b640dec717e03bb1
Sarita Chaudhary <schaudhary@fmgl.com.au>
2022-10-19 10:05:12 +0800

    updated shell scripts

--- docker/script/entrypoint.sh
+++ docker/script/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 TRY_LOOP="20"
 
--- docker/script/generate_key.sh
+++ docker/script/generate_key.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 # Generate fernet key
 FERNET_KEY="$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")"
